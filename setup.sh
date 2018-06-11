f0df2ceca2689273acfbb07be663dca4288f3cd7
syatyo <yoshiharuseijin@me.com>
2018-06-11 21:40:40 +0900

    [feat]: add auto setup shell

---
+++ setup.sh
@@ -0,0 +1,7 @@
+echo "Homebrewのインストール"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "brew bunldeの実行"
+brew bundle
+
+echo "Hello!My new iMac!"
